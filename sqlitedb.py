d45531c1df8e2c4ff721165e9fee5b5f6e016519
Cyrus-spc-tech <vinodgupta12389@gmail.com>
2026-04-13 23:00:28 +0530

    CLass for sqlite obj

--- sqlitedb.py
+++ sqlitedb.py
@@ -9,3 +9,23 @@ cur.execute(f"{userquery}")
 con.commit()
 con.close()
 
+
+q1="Create table {tname} ({colstr});"
+q2="Select * from {tname};"
+q3="Describe {tname};"
+q4="Show Tables;"
+
+class DDB:
+
+    def __init__(self):
+        db=sqlite3.connect('eg.db')
+        cur=db.cursor()
+        cur.execute("CREATE DATABASE IF NOT EXIST sampler ")
+
+    # def fetch(self,db,table):
+
+
+
+
+    
+
